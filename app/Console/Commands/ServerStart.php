2a922bee6d772353d2f65693aaad82141766a0aa
Oliver Lumby <me@oliverlumby.com>
2016-08-15 17:56:03 +0200

    initial

---
+++ app/Console/Commands/ServerStart.php
@@ -0,0 +1,42 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Process\Process;
+
+class ServerStart extends Command {
+    
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'server:start 
+                            {--s|surveil : Whether surveil should also start}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Start game server";
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        $path = base_path('sample_log.log');
+
+        $output = $this->output;
+
+        (new Process('tail -f '.escapeshellarg($path)))->setTimeout(null)->run(function($type, $line) use ($output)
+        {
+            $output->write("Line: " . $line);
+        });
+
+        
+    }
+}
