1979890864635936a0ae85177acb244e99b868a1
Oliver Lumby <me@oliverlumby.com>
2016-08-17 01:55:16 +0200

    clean up use

--- app/Console/Commands/Server/ServerStart.php
+++ app/Console/Commands/Server/ServerStart.php
@@ -2,10 +2,6 @@
 
 namespace App\Console\Commands\Server;
 
-use App\Exceptions\InvalidServerException;
-use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Log;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Process\Process;
 
 class ServerStart extends ServerCommand {
