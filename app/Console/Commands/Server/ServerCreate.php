d117ea5123e4166bbd82840c3d972029f4811148
Oliver Lumby <me@oliverlumby.com>
2016-08-23 20:04:19 +0200

    allow server to be created in single command

--- app/Console/Commands/Server/ServerCreate.php
+++ app/Console/Commands/Server/ServerCreate.php
@@ -9,7 +9,16 @@ class ServerCreate extends ServerCommand {
      *
      * @var string
      */
-    protected $signature = 'server:create';
+    protected $signature = 'server:create
+                            {serverName? : The server name}
+                            {serverPath? : The server path}
+                            {serverBinary? : The server binary}
+                            {serverGame? : The server game}
+                            {serverIp? : The server ip}
+                            {serverPort? : The server port}
+                            {serverRcon? : The server rcon}
+                            {serverParams? : The server params}
+                        ';
 
     /**
      * The console command description.
@@ -25,16 +34,25 @@ class ServerCreate extends ServerCommand {
      */
     public function fire()
     {
-        $server['name'] = $this->collectServerName();
-        $server['path'] = $this->collectPath();
-        $server['binary'] = $this->collectBinary($server['path']);
-        $server['game'] = $this->choice('Server Game', ['cod4', 'cod4x', 'arma3']);
-        $server['ip'] = $this->ask('Server IP', '127.0.0.1');
-        $server['port'] = $this->ask('Server Port');
-        $server['rcon'] = $this->secret('Server Rcon Password');
-        $server['params'] = $this->ask('Server Startup Parameters');
+        $server['name'] = $this->collectConfiguration($this->argument('serverName'), function() { return $this->collectServerName(); });
+        $server['path'] = $this->collectConfiguration($this->argument('serverPath'), function() { return $this->collectPath(); } );
+        $server['binary'] = $this->collectConfiguration($this->argument('serverBinary'), function() use ($server) { return $this->collectBinary($server['path']); } );
+        $server['game'] = $this->collectConfiguration($this->argument('serverGame'), function() { return $this->choice('Server Game', ['cod4', 'cod4x', 'arma3']); } );
+        $server['ip'] = $this->collectConfiguration($this->argument('serverIp'), function() { return $this->ask('Server IP', '127.0.0.1'); } );
+        $server['port'] = $this->collectConfiguration($this->argument('serverPort'), function() { return $this->ask('Server Port'); } );
+        $server['rcon'] = $this->collectConfiguration($this->argument('serverRcon'), function() { return $this->secret('Server Rcon Password'); } );
+        $server['params'] = $this->collectConfiguration($this->argument('serverParams'), function() { return $this->ask('Server Startup Parameters'); } );
 
         $this->createServer($server);
+    }
+
+    protected function collectConfiguration($argument, $else)
+    {
+        if ($argument) {
+            return $argument;
+        }
+
+        $else();
     }
 
     protected function collectServerName()
@@ -45,7 +63,7 @@ class ServerCreate extends ServerCommand {
             $default = 'default';
         } 
 
-        $serverName = $this->ask('Server ID', $default);
+        $serverName = $this->ask('Server Name', $default);
 
         if ($this->server->where('name', $serverName)->count()) {
             $this->error('Server id taken, try again');
