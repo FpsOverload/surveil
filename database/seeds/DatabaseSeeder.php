2917adf273565d120aada8f0accd446eccba578e
Oliver Lumby <me@oliverlumby.com>
2016-08-24 17:32:51 +0200

    seed default supervisor options

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        DB::table('options')->insert([
+            ['option' => 'supervisor_prefix', 'value' => 'surveil_'],
+            ['option' => 'supervisor_user', 'value' => 'oliver'],
+            ['option' => 'supervisor_config', 'value' => '/etc/supervisor/conf.d/surveil.conf']
+        ]);
     }
 }
