14b14a0a037a9d4205896e98a6f5a6a3f08f7ce6
Oliver Lumby <me@oliverlumby.com>
2016-08-29 18:29:00 +0200

    override newQuery to pull in default/active configuration for game server

--- app/Server.php
+++ app/Server.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Server extends Model
 {
@@ -24,6 +25,16 @@ class Server extends Model
     protected $casts = [
         'surveil' => 'boolean',
     ];
+
+    public function newQuery($excludeDeleted = true)
+    {
+        $query = parent::newQuery($excludeDeleted);
+
+        $query->select('servers.*', DB::Raw('IFNULL(configurations.params, servers.default_params) as params'), DB::Raw('IFNULL(configurations.surveil, servers.default_surveil) as surveil'));
+        $query->LeftJoin('configurations', 'servers.active_config', '=', 'configurations.id');
+
+        return $query;
+    }
 
     public function configs()
     {
